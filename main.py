c1495b182bd54b981802a5b10996da9e8d915bda
Sam March <73301871+sam-march@users.noreply.github.com>
2022-03-23 16:30:55 +0000

    Added more commands

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 import discord
 from discord import Member
-
 from discord.utils import get
+
 import random
 import asyncio
 import time
+
 from discord.ext import commands
 
 token = os.environ['token']
@@ -39,20 +40,6 @@ async def hello(ctx):
 	em.set_image(url="attachment://499.png")
 	await ctx.reply(embed=em, mention_author=False, file=file)
 
-@client.command()
-@commands.has_role(935538300570185740)
-async def setup(ctx):
-	msg = await ctx.reply("> Setting Up Bot | Please wait", mention_author=False)
-	msg = msg.id
-	try:
-		guild = client.get_guild(905684366620000287)
-		await guild.create_category("Bot Moderation")
-		 # <-- insert yor guild id here
-		mod_logs = await ctx.guild.create_text_channel("mod logs", category="Bot Moderation")
-		bot_updates = await ctx.guild.create_text_channel("bot updates", category="Bot Moderation")
-        
-	except Exception as errors:
-		await ctx.reply(f"> ❌ | Bot Error: {errors}", mention_author=False)
 
 
 @client.command()
@@ -71,7 +58,7 @@ async def github(ctx):
 	await ctx.reply(embed=em, mention_author=False)
 @client.command()
 async def version(ctx):
-	em = discord.Embed(title="Versions", description="Python:\n`3.8.12`\n\ndiscord.py:\n`Development Version`" ,colour=discord.Colour.purple())
+	em = discord.Embed(title="Versions", description="Python:\n`3.8.12`\ndiscord.py:\n`Development Version`" ,colour=discord.Colour.purple())
 	await ctx.reply(embed=em, mention_author=False)
 
 @client.command()
@@ -79,4 +66,30 @@ async def dev(ctx):
 	dev = client.get_user(723569355710922802)
 	em = discord.Embed(title="About Developer", description=f"This bot was developed by {dev.mention}. If you wish to get in contact, do DM him", colour=discord.Colour.purple())
 	await ctx.reply(embed=em, mention_author=False)
+@client.command()
+async def about(ctx):
+	dev = client.get_user(723569355710922802)
+	em = discord.Embed(title="About Bot", description=f"I was developed by {dev.mention}, and I joined on the <t:1647964800:d>", colour=discord.Colour.purple())
+	await ctx.reply(embed=em, mention_author=False)
+@client.command()
+async def timestamp(ctx):
+	dev = client.get_user(723569355710922802)
+	em = discord.Embed(title="Timestamp Generator", description=f"We all know how hard Discord timestamps are to write, so follow this handy link to quickly create them without the faff\nhttps://bchaing.github.io/discord-timestamp/", colour=discord.Colour.purple())
+	await ctx.reply(embed=em, mention_author=False)
+@client.command()
+async def invite(ctx):
+	dev = client.get_user(723569355710922802)
+	em = discord.Embed(title="Invite Bot", description=f"Unfortunately", colour=discord.Colour.purple())
+	await ctx.reply(embed=em, mention_author=False)
+
+
+
+
+
+
+
+
+
+
+
 client.run(token)
